3ae6ff8cddd526ac28d5d4d980ea936c78577c8b
divyaalakute <107129260+divyaalakute@users.noreply.github.com>
2024-08-18 17:52:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function appendValue(value) {
+    document.calculator.display.value += value;
+}
+
+function deleteLastChar() {
+    const displayValue = document.calculator.display.value;
+    document.calculator.display.value = displayValue.slice(0, -1);
+}
+
+function resetCalculator() {
+    document.calculator.display.value = '';
+}
+
+function calculateResult() {
+    try {
+        document.calculator.display.value = eval(document.calculator.display.value);
+    } catch (error) {
+        document.calculator.display.value = 'Error';
+    }
+}
